8bc510dcfbf5a7a9e2b8d48ae34cf9015fd453b4
royvardhan <royvardhan.official@gmail.com>
2022-08-04 11:30:00 +0530

    Update deploy.js

--- ethers-simple-storage/deploy.js
+++ ethers-simple-storage/deploy.js
@@ -6,10 +6,9 @@ async function main() {
   const provider = new ethers.providers.JsonRpcProvider(
     "http://127.0.0.1:7545"
   );
-  const wallet = new ethers.Wallet(
-    "9168d8020ae088989fe3bcff33b1bcbd0a0b42921c926b41ebe9fa8ffd24d15e",
-    provider
-  );
+  const ganachePrivateKey =
+    "9168d8020ae088989fe3bcff33b1bcbd0a0b42921c926b41ebe9fa8ffd24d15e";
+  const wallet = new ethers.Wallet(ganachePrivateKey, provider);
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8");
   const binary = fs.readFileSync(
     "./SimpleStorage_sol_SimpleStorage.bin",
